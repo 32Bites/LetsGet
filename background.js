97328a407eec5eb4d2dbf7707800d5bb53bb7b18
Noah Shanaberger <noah@thenoah.party>
2021-10-09 18:53:53 -0700

    Fixed an oopsie
    I forgot I was writing a line of code... oof.

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
-chrome.storage.sync.get('enabled', function(data) {
-    if (data.enabled === undefined) {
-        chrome.storage.sync.set({enabled: true}, null);
+chrome.storage.sync.get('enabled', function (data) {
+    if (data === undefined || data.enabled === undefined) {
+        chrome.storage.sync.set({
+            enabled: true
+        }, null);
     }
 });
 
